9b86ae2ffa769db3c9bed4bde2cc9bf44e1d5715
SamedhG <gupta.sam@husky.neu.edu>
2020-03-30 12:20:07 -0400

    added some documentation for the application

--- src/application.rs
+++ src/application.rs
@@ -1,5 +1,15 @@
-use crate::dataframe::DataFrame;
-use crate::dataframe::Rower;
+//! This module defines an application the highest level component of a liquid_ml system. The
+//! application exposes a KVStore and a blob receiver that can be used to send random blocs across
+//! the network. The blob receiver is designed to be used for control messages. 
+//!
+//! A user of the liquid_ml system need only instantiate an application and provide it an async
+//! function to be run. The application grants access to its node_id so different tasks can be done
+//! on different nodes.
+//!
+//! Detailed examples that use the application can be found in the examples directory of this
+//! crate.  
+
+use crate::dataframe::{DataFrame, Rower};
 use crate::error::LiquidError;
 use crate::kv::{KVStore, Key, Value};
 use bincode::{deserialize, serialize};
@@ -9,15 +19,25 @@ use std::future::Future;
 use std::sync::Arc;
 use tokio::sync::{mpsc, mpsc::Receiver, Notify};
 
+/// Represents an application
 pub struct Application {
+    /// A pointer to the KVStore that stores all the data for the application
     pub kv: Arc<KVStore>,
+    /// The id of this node, assigned by the registration server
     pub node_id: usize,
+    /// A receiver for blob messages that can b processed by the user 
     pub blob_receiver: Receiver<Value>,
+    /// the number of nodes in this network
+    /// NOTE: Currently panics if the network is inconsistent with this network
     num_nodes: usize,
+    /// A notifier that gets notified when the server has sent a kill message
     kill_notifier: Arc<Notify>,
 }
 
 impl Application {
+    /// Create a new liquid_ml application
+    /// Needs the address at which the server is listening and its own address along with how many
+    /// nodes are on this network
     pub async fn new(
         my_addr: &str,
         server_addr: &str,
@@ -42,6 +62,8 @@ impl Application {
         })
     }
 
+    /// Create a new application and split the given SoR file across all the nodes in the network
+    /// NOTE: Untested and incomplete
     pub async fn from_sor(
         file_name: &str,
         my_addr: &str,
@@ -64,7 +86,7 @@ impl Application {
         Ok(app)
     }
 
-    ///
+    /// NOTE: In development: This function is currently untested
     ///
     /// NOTE:
     ///
@@ -110,6 +132,9 @@ impl Application {
         }
     }
 
+    /// Given a function run it on this application. This function only terminates when a kill
+    /// signal from the server has been sent. `examples/demo_client.rs` is a good starting point to
+    /// see this in action
     pub async fn run<F, Fut>(self, f: F)
     where
         Fut: Future<Output = ()>,
