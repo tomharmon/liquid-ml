5e006d3115865c9802af36b23b85753f0a60353c
SamedhG <gupta.sam@husky.neu.edu>
2020-03-30 12:43:36 -0400

    Added better argument parsing to the binary

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,9 +1,24 @@
+//! This binary is packaged with the liquid_ml application and is a default implementation of a
+//! registration server. This needs to be running to facilitate connections between differnt nodes
+//! in the system.  
+
 use liquid_ml::error::LiquidError;
 use liquid_ml::network::Server;
+use clap::Clap;
 
+/// This is a simple registration server for a liquid_ml system and comes packaged with the
+/// liquid_ml system. Refer to docs.rs/liquid_ml for further information.   
+#[derive(Clap)]
+#[clap(version = "1.0", author = "Samedh G. & Thomas H.")]
+struct Opts {
+    /// The IP at which this server must run 
+    #[clap(short = "i", long = "ip", default_value = "127.0.0.1:9000")]
+    ip: String,
+}
 #[tokio::main]
 async fn main() -> Result<(), LiquidError> {
-    let mut s = Server::new("127.0.0.1:9000".to_string()).await?;
+    let opts : Opts = Opts::parse();
+    let mut s = Server::new(opts.ip).await?;
     s.accept_new_connections().await?;
     Ok(())
 }
